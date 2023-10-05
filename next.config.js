eddffe5d27f9a06c5992966b13bbfefcba0bf8e4
Cadu <cadu@2km.com.br>
2023-10-05 09:54:42 -0300

    fix: basepath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
+  basePath: isProd ? "/next-deploy/" : "",
 };
 
 module.exports = nextConfig;
